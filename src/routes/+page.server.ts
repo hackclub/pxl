29553d7dad701a9fa2cb3af2abdfa2bd65d6d9ae
Nico Smith <web@niiccoo2.xyz>
2025-12-25 13:19:45 -0500

    Fix var mismatches on homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,18 +3,18 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth();
-	const logged_in = Boolean(session?.user?.email);
-	var is_Beta_Tester: boolean;
+	const signedIn = Boolean(session?.user?.email);
+	let isBetaTester: boolean;
 
 	if (session?.user?.email) { // REMOVE AFTER BETA
 		const user = await getUserFromEmail(session.user.email);
-		is_Beta_Tester = user?.is_Beta_Tester ?? false;
+		isBetaTester = user?.is_Beta_Tester ?? false;
 	} else {
-		is_Beta_Tester = false;
+		isBetaTester = false;
 	}
 
 	return {
-		logged_in,
-		is_Beta_Tester // REMOVE AFTER BETA
+		signedIn,
+		isBetaTester // REMOVE AFTER BETA
 	};
 };
